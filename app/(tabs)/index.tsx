71f21e2a0d3d13aeaae855b4dc37f1bc81a45fa8
Nicolás Mozo <nm@driveoscar.com>
2026-03-03 16:47:57 +0100

    Refactor HomeScreen by removing HelloWave component and unused imports; delete unused ExternalLink, HelloWave, and Collapsible components.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,6 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
@@ -18,10 +17,9 @@ export default function HomeScreen() {
       }>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome to the Algolia POC!</ThemedText>
-        <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        
+
       </ThemedView>
     </ParallaxScrollView>
   );
